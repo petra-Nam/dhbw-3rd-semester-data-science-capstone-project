11b0c793c642250d65be4d20bb64673728bd2dc9
Ngoc901 <huanikii@gmail.com>
2025-11-02 10:46:33 +0100

    Changes

--- utils.py
+++ utils.py
@@ -3,20 +3,30 @@ import pandas as pd
 
 def save_dataframe(df: pd.DataFrame, name: str, folder: str = "data"):
     """
-    Save a DataFrame as a CSV inside the project root data folder.
-    Works even if the notebook runs from a subfolder (e.g., /notebooks).
+    Save a DataFrame as a CSV
     """
-    # Always create path relative to project root
-    root_path = os.path.dirname(os.getcwd())  # go one level up
+    # Go one level up from notebooks (to project root)
+    root_path = os.path.dirname(os.getcwd())
+
+    # Get the project folder name ('capstone-project')
+    project_name = os.path.basename(root_path)
+
+    # Construct data folder path
     data_path = os.path.join(root_path, folder)
     os.makedirs(data_path, exist_ok=True)
 
+    # Full file path
     path = os.path.join(data_path, f"{name}.csv")
 
+    # Display-friendly path (capstone-project/data/eda_output.csv)
+    display_path = os.path.join(project_name, folder, f"{name}.csv")
+
+
     if os.path.exists(path):
-        print(f"File already exists. Overwriting {path}")
+        print(f"File already exists. Overwriting {display_path}")
     else:
-        print(f"Creating new file {path}")
+        print(f"Creating new file {display_path}")
+
 
     df.to_csv(path, index=False)
     print("DataFrame saved successfully.")
